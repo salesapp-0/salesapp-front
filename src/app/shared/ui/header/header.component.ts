55a01a040d9b918b6b3fadc1163057ee0700adaf
LukaTypeScripter <lukashinjikashvili85@gmail.com>
2024-09-07 21:27:07 +0400

    feat:styles

--- src/app/shared/ui/header/header.component.ts
+++ src/app/shared/ui/header/header.component.ts
@@ -1,23 +1,25 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {AuthService} from "../../services/auth.service";
-import {map, Observable} from "rxjs";
-import {User} from "../../../core/interfaces/user.interface";
-import {CommonModule} from "@angular/common";
+import { Component, inject, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { map, Observable } from 'rxjs';
+import { User } from '../../../core/interfaces/user.interface';
+import { CommonModule } from '@angular/common';
+import { NavigateService } from '../../services/navigate.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent implements OnInit{
-  private authService = inject(AuthService)
-  user$!:Observable<User>
+export class HeaderComponent implements OnInit {
+  private authService = inject(AuthService);
+  private navigateService = inject(NavigateService);
+  user$!: Observable<User>;
   ngOnInit() {
-   this.user$ = this.authService.getUser$().pipe(
-      map((userData) => userData)
-    )
+    this.user$ = this.authService.getUser$().pipe(map((userData) => userData));
   }
-
+  onNavigate(url: string) {
+    this.navigateService.navigateTo(url);
+  }
 }
