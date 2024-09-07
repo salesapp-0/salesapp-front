cbb95471029862d2521f1d7ae4fdf826d24bad89
LukaTypeScripter <lukashinjikashvili85@gmail.com>
2024-09-07 19:17:36 +0400

    format

--- src/app/shared/services/organization-helper.service.ts
+++ src/app/shared/services/organization-helper.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
-import {map, takeUntil} from "rxjs";
-import {OrganizationsService} from "./organizations.service";
-import {FormGroup} from "@angular/forms";
-import {BuyerOrganization} from "../../core/interfaces/buyer-organizations.interface";
+import { map, takeUntil } from 'rxjs';
+import { OrganizationsService } from './organizations.service';
+import { FormGroup } from '@angular/forms';
+import { BuyerOrganization } from '../../core/interfaces/buyer-organizations.interface';
 import { EventEmitter } from 'stream';
 
 @Injectable()
 export class OrganizationHelperService {
-
   constructor(private organizationService: OrganizationsService) {}
 
   getOrganizationFormData(organizationForm: FormGroup): any {
@@ -23,48 +22,63 @@ export class OrganizationHelperService {
       bankAccountNumber: organizationForm.get('bankAccountNumber')?.value,
       contactPerson: organizationForm.get('contactPerson')?.value,
       contactPhoneNumber: organizationForm.get('contactPhoneNumber')?.value,
-      contactMail: organizationForm.get('contactMail')?.value
+      contactMail: organizationForm.get('contactMail')?.value,
     };
   }
 
-  createOrganization(organizationData: BuyerOrganization, unsubscribe$: any,close:any): void {
-    this.organizationService.createBuyerOrganization(
-      organizationData.legalName,
-      organizationData.tradeName,
-      organizationData.identificationCode,
-      organizationData.legalAddress,
-      organizationData.actualAddress,
-      organizationData.serviceCost,
-      organizationData.paymentDate,
-      organizationData.bank,
-      organizationData.bankAccountNumber,
-      organizationData.contactPerson,
-      organizationData.contactPhoneNumber,
-      organizationData.contactMail
-    ).pipe(
-      map((res) => close),
-      takeUntil(unsubscribe$)
-    ).subscribe();
+  createOrganization(
+    organizationData: BuyerOrganization,
+    unsubscribe$: any,
+    close: any
+  ): void {
+    this.organizationService
+      .createBuyerOrganization(
+        organizationData.legalName,
+        organizationData.tradeName,
+        organizationData.identificationCode,
+        organizationData.legalAddress,
+        organizationData.actualAddress,
+        organizationData.serviceCost,
+        organizationData.paymentDate,
+        organizationData.bank,
+        organizationData.bankAccountNumber,
+        organizationData.contactPerson,
+        organizationData.contactPhoneNumber,
+        organizationData.contactMail
+      )
+      .pipe(
+        map((res) => close),
+        takeUntil(unsubscribe$)
+      )
+      .subscribe();
   }
 
-  updateOrganization(organizationData: BuyerOrganization, organizationId: string, unsubscribe$: any,close:any): void {
-    this.organizationService.updateBuyerOrganization(
-      organizationData.legalName,
-      organizationData.tradeName,
-      organizationData.identificationCode,
-      organizationData.legalAddress,
-      organizationData.actualAddress,
-      organizationData.serviceCost,
-      organizationData.paymentDate,
-      organizationData.bank,
-      organizationData.bankAccountNumber,
-      organizationData.contactPerson,
-      organizationData.contactPhoneNumber,
-      organizationData.contactMail,
-      organizationId
-    ).pipe(
-      map((res) =>close),
-      takeUntil(unsubscribe$)
-    ).subscribe();
+  updateOrganization(
+    organizationData: BuyerOrganization,
+    organizationId: string,
+    unsubscribe$: any,
+    close: any
+  ): void {
+    this.organizationService
+      .updateBuyerOrganization(
+        organizationData.legalName,
+        organizationData.tradeName,
+        organizationData.identificationCode,
+        organizationData.legalAddress,
+        organizationData.actualAddress,
+        organizationData.serviceCost,
+        organizationData.paymentDate,
+        organizationData.bank,
+        organizationData.bankAccountNumber,
+        organizationData.contactPerson,
+        organizationData.contactPhoneNumber,
+        organizationData.contactMail,
+        organizationId
+      )
+      .pipe(
+        map((res) => close),
+        takeUntil(unsubscribe$)
+      )
+      .subscribe();
   }
 }
