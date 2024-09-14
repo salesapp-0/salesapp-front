2085433f4535dbc432e2b3ed43c04644de8b68c2
LukaTypeScripter <lukashinjikashvili85@gmail.com>
2024-09-14 16:59:18 +0400

    feat:clean

--- src/app/shared/services/envirment.service.ts
+++ src/app/shared/services/envirment.service.ts
@@ -1,10 +1,10 @@
-import {Inject, Injectable} from '@angular/core';
-import {ENV_CONFIG, EnvironmentConfig} from "../../core/configs/environment.config";
+import { Inject, Injectable } from '@angular/core';
+import {
+  ENV_CONFIG,
+  EnvironmentConfig,
+} from '../../core/configs/environment.config';
 
 @Injectable()
 export class EnvironmentService {
-
-  constructor(@Inject(ENV_CONFIG) public readonly config: EnvironmentConfig) {
-    console.log(config)
-  }
+  constructor(@Inject(ENV_CONFIG) public readonly config: EnvironmentConfig) {}
 }
